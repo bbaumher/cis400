df957991451fba1b35789ce467b096f9926fca67
Scott Buchanan <scob@seas.upenn.edu>
2015-11-11 00:32:31 -0500

    commented my code

--- ReferralLog.java
+++ ReferralLog.java
@@ -1,32 +1,61 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author Scott
+ *
+ * The ReferralLog class maintains a map from Node to int[].
+ * Each entry of the int[] corresponds to one of s's neighbors.
+ * The value of the entry represents how many shortest paths there are
+ * from s to that node which pass through the neighbor of s represented
+ * by that entry.
+ * 
+ * For example, if node v has array [0 2 1], then there are:
+ * - 0 shortest path(s) from s to v that pass through neighbor 0
+ * - 2 shortest path(s) from s to v that pass through neighbor 2
+ * - 1 shortest path(s) from s to v that pass through neighbor 1
+ */
+
 public class ReferralLog {
 	
-	private final int numNeighbors;
-	Map<Node,int[]> referralMap;
+	private final int numNeighbors; //the number of neighbors s has
+	Map<Node,int[]> referralMap; //the actual mapping from Node to int[]
 	
+	/** Construct a ReferralLog by storing the number of neighbors and
+	 *  instantiating the map.
+	 * 
+	 * @param numNeighbors the number of neighbors s has
+	 */
 	public ReferralLog (int numNeighbors) {
 		this.numNeighbors = numNeighbors;
 		referralMap = new HashMap<Node,int[]>();
 	}
 	
+	/** Get the referral of a node, i.e. its mapped to int[].
+	 *  If it doesn't have one yet, then an empty one is created,
+	 *  added to the mapping, and returned.
+	 */
 	public int[] getReferral(Node node) {
-		if (referralMap.containsKey(node)) {
+		if (referralMap.containsKey(node)) { //node already has a referral array
 			return referralMap.get(node);
 		}
-		else {
+		else { //node doesn't have a referral array yet
 			int[] emptyReferral = new int[numNeighbors];
 			referralMap.put(node, emptyReferral);
 			return emptyReferral;
 		}
 	}
 	
+	/** Set the index'th entry of node's referral array to value.
+	 */
 	public void setValue(Node node, int index, int value) {
 		int[] values = getReferral(node);
 		values[index] = value;
 	}
 	
+	/** Add the values of node1's referral array onto the
+	 *  referral array of node2.
+	 */
 	public void addValuesOnto(Node node1, Node node2) {
 		int[] node1ref = getReferral(node1);
 		int[] node2ref = getReferral(node2);
@@ -35,6 +64,9 @@ public class ReferralLog {
 		}
 	}
 	
+	/** Represent this class by showing the int[] corresponding
+	 *  to each of the nodes.
+	 */
 	@Override
 	public String toString() {
 		StringBuffer buf = new StringBuffer();
--- Tester.java
+++ Tester.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
 public class Tester {
 	
-
+	/** Run the algorithm on a test graph.
+	 */
 	public static void main(String[] args) {
+		//create nodes
 		int s = 0;
 		int a = 1;
 		int b = 2;
@@ -22,7 +22,11 @@ public class Tester {
 		int i = 9;
 		int j = 10;
 		int k = 11;
+		
+		//create graph
 		AdjListGraph testGraph = new AdjListGraph(12);
+		
+		//add edges to the graph
 		testGraph.addEdge(s,a);
 		testGraph.addEdge(s,b);
 		testGraph.addEdge(s,c);
@@ -39,76 +43,126 @@ public class Tester {
 		testGraph.addEdge(g,j);
 		testGraph.addEdge(i,k);
 		
+		//run the algorithm
 		Node nodeS = testGraph.getNode(s);
-		run(3, nodeS);
+		runAlgorithm(nodeS, 3);
 	}
 	
-	public static void run(int k, Node s) {
-		Set<Node> A = new HashSet<Node>();
-		Queue<Node> Q = new LinkedList<Node>();
-		Set<Node> C = new HashSet<Node>();
-		int neighborCount = s.getAdj().size();
-		ArrayList<Set<Node>> nodeTiers = new ArrayList<Set<Node>>();
-		ReferralLog referralLog = new ReferralLog(neighborCount);
+	/**
+	 * The k credit algorithm.
+	 * 
+	 * @param s the starting node
+	 * @param k we consider nodes up to k away from the starting node
+	 */
+	public static void runAlgorithm(Node s, int k) {
+		Set<Node> seenNodes = new HashSet<Node>(); //the nodes that no longer need to be considered
+		Queue<Node> nodeQueue = new LinkedList<Node>(); //the nodes on the current layer of the BFS
+		Set<Node> pendingNodes = new HashSet<Node>(); //the the nodes being discovered on the edge of the BFS
+		int neighborCount = s.getAdj().size(); //the number of neighbors of the starting node
+		ArrayList<Set<Node>> nodeTiers = new ArrayList<Set<Node>>(); //a list of sets of nodes, each set containing nodes i away from s
+		ReferralLog referralLog = new ReferralLog(neighborCount); //a map from each node to its referral array
 		
+		//give each of s's neighbors a 1 in its corresponding referral array
 		for (int i = 0; i < s.getAdj().size(); i++) {
 			Node v = s.getAdj().get(i);
 			referralLog.setValue(v, i, 1);
-			C.add(v);
+			pendingNodes.add(v);
 		}
 		
-		A.add(s);
+		//perform logistics on s as the zeroth tier
+		seenNodes.add(s);
 		Set<Node> zerothTier = new HashSet<Node>();
 		zerothTier.add(s);
 		nodeTiers.add(zerothTier);
 		
+		//perform a BFS, filling out the nodeTiers and keeping track of referrals
 		for (int i = 0; i < k-1; i++) {
-			A.addAll(C);
-			Q.addAll(C);
-			nodeTiers.add(C);
-			C = new HashSet<Node>();
+			seenNodes.addAll(pendingNodes); //archive the nodes seen last round
+			nodeQueue.addAll(pendingNodes); //the nodes discovered last round are next
+			nodeTiers.add(pendingNodes); //add the pending nodes to the nodeTiers
+			pendingNodes = new HashSet<Node>(); //reset the pending nodes
 			
-			while (!Q.isEmpty()) {
-				Node v = Q.poll();
+			//iterate through each node in the queue
+			while (!nodeQueue.isEmpty()) {
+				Node v = nodeQueue.poll();
+				
+				//examine each of the node's neighbors
 				for (Node u : v.getAdj()) {
-					if (A.contains(u)) continue;
-					else {
-						C.add(u);
-						referralLog.addValuesOnto(v,u);
+					if (seenNodes.contains(u)) continue; //if we've seen it before, skip it
+					else {//otherwise, we haven't seen it yet
+						pendingNodes.add(u); //add it to the pending nodes (possibly redundantly)
+						referralLog.addValuesOnto(v,u); //propagate the referrals
 					}
 				}
 			}
 		}
 		
-		nodeTiers.add(C);
+		//add the final tier to the nodeTiers
+		nodeTiers.add(pendingNodes);
 		
+		//perform some algorithm to determine credits
 		double[] p = calculateCredits(nodeTiers, referralLog, neighborCount, k);
 		
+		//output the display
 		for (int i = 0; i < p.length; i++) {
 			System.out.println(p[i]);
 		}
 	}
 	
+	/**
+	 * Perform the k credit algorithm.
+	 * 
+	 * @param nodeTiers a list of sets of nodes, each set containing nodes i away from s
+	 * @param referralLog a map from each node to its referral array
+	 * @param neighborCount the number of neighbors of the starting node
+	 * @param k we consider nodes up to k away from the starting node
+	 * @return the probability distribution for going to s's neighbors
+	 */
 	private static double[] calculateCredits(ArrayList<Set<Node>> nodeTiers, 
 			ReferralLog referralLog, int neighborCount, int k) {
-		double[] p = new double[neighborCount];
-		Set<Node> edgeNodes = nodeTiers.get(k);
+		double[] p = new double[neighborCount]; //instantiate the output array
+		Set<Node> edgeNodes = nodeTiers.get(k); //get the final tier (i.e. nodes exactly k away from s)
+		
+		//each node has 1 credit, which is split proportionally to its referrals
 		for (Node v : edgeNodes) {
 			int[] referrals = referralLog.getReferral(v);
 			for (int i = 0; i < referrals.length; i++) {
-				int sum = sum(referrals);
-				p[i] += referrals[i] / (double) sum;
+				double sum = sum(referrals);
+				p[i] += referrals[i] / sum;
 			}
 		}
+		normalize(p); //normalize the probability vector
 		return p;
 	}
 	
+	/** Return the sum of an integer array.
+	 */
 	private static int sum(int[] array) {
 		int sum = 0;
 		for (int entry : array) {
 			sum += entry;
 		}
 		return sum;
+	}
+	
+	/** Return the sum of a double array.
+	 */
+	private static double sum(double[] array) {
+		double sum = 0;
+		for (double entry : array) {
+			sum += entry;
+		}
+		return sum;
+	}
+	
+	/** Normalize an array by scaling each of its entries so that
+	 *  the sum becomes 1. Modifies the array in place.
+	 */
+	private static void normalize(double[] array) {
+		double sum = sum(array);
+		for (int i = 0; i < array.length; i++) {
+			array[i] = array[i] / sum;
+		}
 	}
 
 
